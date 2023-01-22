16e695fb3fba6ae46bb6b1bedc00708b5d8f5947
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-22 22:22:24 +0100

    Added better use of variables as was suggested in the first review

--- main.py
+++ main.py
@@ -12,28 +12,30 @@ goal_1 = 45 + 9
 
 scorers = player_first_scored + " " + str(goal_0) + ", " +  player_second_scored + " " + str(goal_1)
 
-report = (f'{player_first_scored} scored in the {goal_0}nd minute\n{player_second_scored} scored in the {goal_1}th minute')
+report = f'{player_first_scored} scored in the {goal_0}nd minute\n{player_second_scored} scored in the {goal_1}th minute'
 
 print(report)
 
 #Part 2
 
 player = 'Hans van Breukelen'
-first_name =(player[0:4])
-find_player_first_name = str(player.find ("Hans"))
+return_firstname_len = player.find(" ")
+first_name = player[0:return_firstname_len]
+print(first_name)
+last_name_tmp = player[return_firstname_len:]
+last_name = last_name_tmp[1:]
+print (last_name)
 
-player_last_name = (player[5:])
-str(player_last_name)
-
-last_name_len = len(player_last_name)
+last_name_len = len(last_name)
 print (last_name_len)
 
-name_short = ((player[0]) + ". "  + (player_last_name)) 
+name_short = ((player[0]) + ". "  + (last_name)) 
 print (name_short)
 
-chant = str(4 * (first_name+"! "))
-chant = (chant[0:-1])
+#print(f"Hello, {first_name_player}!")
+chant = ((f"{first_name}! ")*return_firstname_len)
+chant = chant[:-1]
 print (chant)
 
-last_character = 32
-good_chant = last_character != ord(chant[-1])
+ASCII_last_character = 32
+good_chant = ASCII_last_character != ord(chant[-1])
